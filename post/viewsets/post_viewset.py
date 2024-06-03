a23b2e94415db394fbc85fac75e1641a2213858a
Lucas Vizentin <Lucas.Vizentin@hotmail.com>
2024-06-03 16:29:56 -0400

    docker compose implementation and workflows automotation

--- post/viewsets/post_viewset.py
+++ post/viewsets/post_viewset.py
@@ -1,13 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 from post.models import Post
 from post.serializers import PostSerializer
 
 class PostViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
     def get_queryset(self):
