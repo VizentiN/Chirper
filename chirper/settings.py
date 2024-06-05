b44f3caaf215afad98bd319da6fc70e2ec01fd7d
Lucas Vizentin <Lucas.Vizentin@hotmail.com>
2024-06-04 21:57:37 -0400

    fast pull request true the server

--- chirper/settings.py
+++ chirper/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'chirper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "chirper", "templates", "authentication", "templates", "post", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- chirper/urls.py
+++ chirper/urls.py
@@ -1,4 +1,5 @@
 import debug_toolbar
+from chirper import views
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -9,7 +10,8 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    # re_path("chirper/(?P<version>(v1|v2))/", include("post.urls")),
     path('post/', include('post.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
